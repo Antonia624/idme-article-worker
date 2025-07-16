52d17b8e169e68c42117e38388ab78529097534e
Antonia624 <160965226+Antonia624@users.noreply.github.com>
2025-07-15 14:56:44 -1000

    Delete _worker.js

--- _worker.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    return new Response("Worker is running — dynamic routes must go through Cloudflare Pages' functions routing.", {
-      status: 404
-    });
-  }
-};
